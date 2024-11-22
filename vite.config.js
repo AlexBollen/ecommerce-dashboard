b3448a2f0356f2a37d2e2410956ce947c89722f5
AlexBollen <alexbollen21@gmail.com>
2024-11-22 01:08:45 -0600

    Update: docker issues

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   define: {
-    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL),
-  }
-})
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || 'http://localhost:3000'),
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      external: [
+        'jsvectormap/dist/jsvectormap.css',
+      ],
+    },
+  },
+});
